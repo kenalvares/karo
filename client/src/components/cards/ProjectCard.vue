2ccd8e0a7707637a772317f2fe1063c13fc05304
Kenneth Alvares <kennethalvares30399@gmail.com>
2020-01-01 18:23:55 +0530

    Added projects view page :sparkles:

---
+++ client/src/components/cards/ProjectCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card max-width="344" class="mx-auto">
+    <v-list-item>
+      <v-list-item-avatar>
+        <v-img :src="project.avatar"></v-img
+      ></v-list-item-avatar>
+      <v-list-item-content>
+        <v-list-item-title class="headline">{{
+          project.title
+        }}</v-list-item-title>
+        <v-list-item-subtitle>{{ project.team }}</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-img :src="project.backgroundUrl" height="194"></v-img>
+
+    <v-card-text>
+      {{ project.description }}
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn text color="deep-purple accent-4">
+        View
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-share-variant</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "ProjectCard",
+  props: {
+    project: Object
+  }
+};
+</script>
---
+++ client/src/components/sheets/CreateProjectDialog.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-bottom-sheet v-model="createProjectDialog">
+    <template v-slot:activator="{ on }">
+      <v-btn outlined color="primary" v-on="on">
+        <v-icon left>add</v-icon>
+        Create Project
+      </v-btn>
+    </template>
+    <v-sheet class="text-center">
+      <v-btn
+        class="mt-6"
+        flat
+        color="red"
+        @click="createProjectDialog = !createProjectDialog"
+        >close</v-btn
+      >
+      <div>
+        <CreateProjectStepper />
+      </div>
+    </v-sheet>
+  </v-bottom-sheet>
+</template>
+
+<script>
+import CreateProjectStepper from "@/components/steppers/CreateProjectStepper";
+export default {
+  name: "CreateProjectDialog",
+  data: () => ({
+    createProjectDialog: false
+  }),
+  components: {
+    CreateProjectStepper
+  }
+};
+</script>
