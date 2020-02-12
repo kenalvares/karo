9b1f7e52928b767cf4db89dd0b5179a6983237b0
Kenneth Alvares <kennethalvares30399@gmail.com>
2020-02-12 18:21:42 +0530

    LoadingData.vue documentation :book:

--- client/src/components/loaders/LoadingData.vue
+++ client/src/components/loaders/LoadingData.vue
@@ -1,6 +1,8 @@
 <template>
+  <!-- If no more data is pending form server, add this row -->
   <v-row v-if="loader.pendingData" class="fill-height flex-column-center">
     <v-col cols="12" align="center" class="flex-column-center">
+      <!-- Indeterminate spinner -->
       <v-progress-circular
         :size="100"
         :width="5"
@@ -8,8 +10,9 @@
         indeterminate
         class="mb-5"
         v-if="!loader.failed"
-      ></v-progress-circular
-      ><v-progress-circular
+      ></v-progress-circular>
+      <!-- Failed spinner -->
+      <v-progress-circular
         :size="100"
         :width="5"
         color="grey"
@@ -18,6 +21,7 @@
       >
         <v-icon color="grey">close</v-icon></v-progress-circular
       >
+      <!-- Msg while data is pending -->
       <span>{{ loader.pendingMsg }}</span>
     </v-col>
   </v-row>
@@ -37,6 +41,11 @@ export default {
   name: "LoadingData",
   props: {
     loader: Object
+    /*
+      pendingData: Boolean,
+      pendingMsg: String,
+      failed: Boolean
+    */
   }
 };
 </script>
