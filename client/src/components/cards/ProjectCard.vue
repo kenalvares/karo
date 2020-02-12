cc0440a8c471e27d77e654a59adb321a8f91ba4b
Kenneth Alvares <kennethalvares30399@gmail.com>
2020-02-12 19:34:25 +0530

    ProjectCard.vue documentation :book:

--- client/src/components/cards/ProjectCard.vue
+++ client/src/components/cards/ProjectCard.vue
@@ -3,22 +3,29 @@
 <template>
   <v-card max-width="344" class="mx-auto">
     <v-list-item>
+      <!-- Team Logo -->
       <v-list-item-avatar v-if="hasTeamAvatar">
         <v-img :src="teamAvatar"></v-img>
       </v-list-item-avatar>
       <v-list-item-content>
+        <!-- Project Name -->
         <v-list-item-title class="headline">
           {{ project.title }}
         </v-list-item-title>
+        <!-- Team Name -->
         <v-list-item-subtitle>{{ project.team }}</v-list-item-subtitle>
       </v-list-item-content>
+      <!-- Project Status -->
       <v-icon right :color="iconColor">{{ projectStatus }}</v-icon>
     </v-list-item>
+    <!-- Project background -->
     <v-img v-if="hasBackground" :src="background" height="194"></v-img>
+    <!-- Project Description -->
     <v-card-text>
       {{ project.description }}
     </v-card-text>
     <v-card-actions>
+      <!-- View Project -->
       <v-btn text color="indigo accent-4" :to="'project/' + project.id">
         View
       </v-btn>
@@ -31,12 +38,43 @@ export default {
   name: "ProjectCard",
   props: {
     project: Object
+    /*
+      id: String,
+      title: String,
+      description: String,
+      backgroundUrl: String,
+      status: String,
+      team: String,
+      avatar: String
+    */
   },
   data: () => ({
-    iconColor: "test",
-    hasTeamAvatar: true,
-    hasBackground: true
+    iconColor: "test", // Project status icon color
+    hasTeamAvatar: true, // Does team have a logo?
+    hasBackground: true // Does project have a background?
   }),
+  created() {
+    // Does project have a background?
+    if (
+      this.project.background === null ||
+      this.project.background === undefined ||
+      this.project.background === ""
+    ) {
+      this.hasBackground = false;
+    } else {
+      this.hasBackground = true;
+    }
+    // Does team have a logo?
+    if (
+      this.project.avatar === null ||
+      this.project.avatar === undefined ||
+      this.project.avatar === ""
+    ) {
+      this.hasTeamAvatar = false;
+    } else {
+      this.hasTeamAvatar = true;
+    }
+  },
   computed: {
     // Set project status icon and color
     projectStatus() {
@@ -50,6 +88,7 @@ export default {
       this.setIconColor("info");
       return "pause";
     },
+    // Return sanitized team avatar url
     teamAvatar() {
       if (
         this.project.avatar === null ||
@@ -60,6 +99,7 @@ export default {
       }
       return this.project.avatar;
     },
+    // Return sanitized background img url
     background() {
       if (
         this.project.background === null ||
@@ -69,27 +109,6 @@ export default {
         return "";
       }
       return this.project.background;
-    }
-  },
-  created() {
-    if (
-      this.project.background === null ||
-      this.project.background === undefined ||
-      this.project.background === ""
-    ) {
-      this.hasBackground = false;
-    } else {
-      this.hasBackground = true;
-    }
-
-    if (
-      this.project.avatar === null ||
-      this.project.avatar === undefined ||
-      this.project.avatar === ""
-    ) {
-      this.hasTeamAvatar = false;
-    } else {
-      this.hasTeamAvatar = true;
     }
   },
   methods: {
